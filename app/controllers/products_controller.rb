65389bae092424f893f4857177f92409368541f4
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-11-02 02:44:40 +0500

    New method for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,16 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+  def product_params
+    params.require(:product).permit(:name)
+  end
 end
