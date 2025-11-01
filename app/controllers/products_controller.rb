a5ed516dc8baf83a14b78d5e62c5b407fc04fd8b
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-11-02 02:54:49 +0500

    Edit and update method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,17 @@ class ProductsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  def edit
+    @product = Product.find(params[:id])
+  end
+  def update
+    @product = Product.find(params(:id))
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
   private
   def product_params
     params.require(:product).permit(:name)
