4a800fcffa9b9b8ff71bb0939941f21bb1e51a9a
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-11-02 14:39:27 +0500

    Rich action text setup done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,7 +41,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name)
+    params.require(:product).permit(:name, :description)
   end
 
   def set_product
