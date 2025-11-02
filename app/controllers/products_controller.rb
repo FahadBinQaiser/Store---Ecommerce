7a7c28347c7edecbbe662f9968d4ea814afdd854
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-11-02 14:56:53 +0500

    Added image to product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,7 +41,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description)
+    params.require(:product).permit(:name, :description, :featured_image)
   end
 
   def set_product
