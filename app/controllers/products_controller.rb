d9b997c100b07867282de81381324c89bbed87a2
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-11-02 13:24:57 +0500

    CRUD done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,17 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :update, :show, :destroy]
+  
   def index
     @products = Product.all
   end
+
   def show
-    @product = Product.find(params[:id])
   end
+
   def new
     @product = Product.new
   end
+
   def create
     @product = Product.new(product_params)
     if @product.save
@@ -16,19 +20,30 @@ class ProductsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
   def edit
-    @product = Product.find(params[:id])
   end
+
   def update
-    @product = Product.find(params(:id))
     if @product.update(product_params)
       redirect_to @product
     else
       render :edit, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path, status: :see_other
+  end
+
   private
+
   def product_params
     params.require(:product).permit(:name)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
